3d12935e25a2f92cce7c2f5543f39b76c2942319
Adrian Machado <machadoadriandev@gmail.com>
2025-08-19 19:10:43 -0300

    section actors

---
+++ src/consts/actors.ts
@@ -0,0 +1,124 @@
+import type { Actors } from "../types/actors";
+
+export const ACTORS:Actors[] = [
+  {
+    id: 1,
+    name: "Florence Pugh",
+    age: 28,
+    occupation: "Actriz",
+    notableWorks: ["Midsommar", "Little Women", "Oppenheimer"],
+    image: "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2025/04/29/17459286646395.jpg"
+  },
+  {
+    id: 2,
+    name: "Timothée Chalamet",
+    age: 29,
+    occupation: "Actor",
+    notableWorks: ["Dune", "Call Me by Your Name", "Wonka"],
+    image: "https://media.vogue.mx/photos/61ae54524e8164be2c15aa6f/2:3/w_2560%2Cc_limit/GettyImages-1347362857.jpg"
+  },
+  {
+    id: 3,
+    name: "Zendaya",
+    age: 28,
+    occupation: "Actriz y cantante",
+    notableWorks: ["Euphoria", "Dune", "Spider-Man"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Zendaya_-_2019_by_Glenn_Francis.jpg/1200px-Zendaya_-_2019_by_Glenn_Francis.jpg"
+  },
+  {
+    id: 4,
+    name: "Anya Taylor-Joy",
+    age: 29,
+    occupation: "Actriz",
+    notableWorks: ["The Queen’s Gambit", "The Witch", "Furiosa"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Anya_Taylor-Joy_2018.jpg"
+  },
+  {
+    id: 5,
+    name: "Jacob Elordi",
+    age: 28,
+    occupation: "Actor",
+    notableWorks: ["Euphoria", "Saltburn", "Priscilla"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/7/71/Jacob_Elordi_2019.jpg"
+  },
+  {
+    id: 6,
+    name: "Florence Kasumba",
+    age: 49,
+    occupation: "Actriz",
+    notableWorks: ["Black Panther", "Wonder Woman", "The Lion King"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Florence_Kasumba_2019.jpg"
+  },
+  {
+    id: 7,
+    name: "Austin Butler",
+    age: 34,
+    occupation: "Actor",
+    notableWorks: ["Elvis", "Dune: Part Two", "Once Upon a Time in Hollywood"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Austin_Butler_2019.jpg"
+  },
+  {
+    id: 8,
+    name: "Jenna Ortega",
+    age: 23,
+    occupation: "Actriz",
+    notableWorks: ["Wednesday", "Scream VI", "X"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Jenna_Ortega_2022.jpg"
+  },
+  {
+    id: 9,
+    name: "Barry Keoghan",
+    age: 32,
+    occupation: "Actor",
+    notableWorks: ["Saltburn", "The Banshees of Inisherin", "Eternals"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Barry_Keoghan_2019.jpg"
+  },
+  {
+    id: 10,
+    name: "Hunter Schafer",
+    age: 27,
+    occupation: "Actriz y modelo",
+    notableWorks: ["Euphoria", "The Hunger Games: The Ballad of Songbirds & Snakes"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Hunter_Schafer_2019.jpg"
+  },
+  {
+    id: 11,
+    name: "Paul Mescal",
+    age: 29,
+    occupation: "Actor",
+    notableWorks: ["Normal People", "Aftersun", "Gladiator II"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Paul_Mescal_2020.jpg"
+  },
+  {
+    id: 12,
+    name: "Sydney Sweeney",
+    age: 28,
+    occupation: "Actriz",
+    notableWorks: ["Euphoria", "The White Lotus", "Anyone But You"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/83/Sydney_Sweeney_2019.jpg"
+  },
+  {
+    id: 13,
+    name: "Josh O’Connor",
+    age: 35,
+    occupation: "Actor",
+    notableWorks: ["The Crown", "Challengers", "Emma"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Josh_O%27Connor_2019.jpg"
+  },
+  {
+    id: 14,
+    name: "Rachel Zegler",
+    age: 24,
+    occupation: "Actriz y cantante",
+    notableWorks: ["West Side Story", "The Hunger Games: The Ballad of Songbirds & Snakes"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Rachel_Zegler_2021.jpg"
+  },
+  {
+    id: 15,
+    name: "Jacob Tremblay",
+    age: 19,
+    occupation: "Actor",
+    notableWorks: ["Room", "Wonder", "The Little Mermaid"],
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d8/Jacob_Tremblay_2019.jpg"
+  }
+];
---
+++ src/types/actors.ts
@@ -0,0 +1,8 @@
+export interface Actors{
+    id:number;
+    name:string;
+    age:number;
+    occupation:string;
+    notableWorks:string[];
+    image:string;
+}
