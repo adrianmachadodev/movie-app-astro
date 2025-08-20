eb758609ad2d6ac7e12c35ed48444d1ee9917b8c
Adrian Machado <machadoadriandev@gmail.com>
2025-08-20 19:07:55 -0300

    section actors carousel

--- src/consts/actors.ts
+++ src/consts/actors.ts
@@ -31,7 +31,7 @@ export const ACTORS:Actors[] = [
     age: 29,
     occupation: "Actriz",
     notableWorks: ["The Queen’s Gambit", "The Witch", "Furiosa"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Anya_Taylor-Joy_2018.jpg"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Anya_Taylor-Joy_by_Patrick_Lovell%2C_January_2019.jpg/1200px-Anya_Taylor-Joy_by_Patrick_Lovell%2C_January_2019.jpg"
   },
   {
     id: 5,
@@ -39,7 +39,7 @@ export const ACTORS:Actors[] = [
     age: 28,
     occupation: "Actor",
     notableWorks: ["Euphoria", "Saltburn", "Priscilla"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/7/71/Jacob_Elordi_2019.jpg"
+    image: "https://cdn.britannica.com/97/258697-050-926B4240/actor-jacob-elordi-at-saltburn-film-premiere-the-theatre-at-ace-hotel-los-angeles-november-14-2023.jpg"
   },
   {
     id: 6,
@@ -47,7 +47,7 @@ export const ACTORS:Actors[] = [
     age: 49,
     occupation: "Actriz",
     notableWorks: ["Black Panther", "Wonder Woman", "The Lion King"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Florence_Kasumba_2019.jpg"
+    image: "https://m.media-amazon.com/images/M/MV5BNDg4NDlhZDQtYWI0OC00YmM4LWFkYzEtMjUwZWYyMWEyNmNmXkEyXkFqcGc@._V1_.jpg"
   },
   {
     id: 7,
@@ -55,7 +55,7 @@ export const ACTORS:Actors[] = [
     age: 34,
     occupation: "Actor",
     notableWorks: ["Elvis", "Dune: Part Two", "Once Upon a Time in Hollywood"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Austin_Butler_2019.jpg"
+    image: "https://m.media-amazon.com/images/M/MV5BYWI2MTc5YmUtMDgyOC00ZjE5LTg5MWYtMTYzNDRlOWI4OTAyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
   },
   {
     id: 8,
@@ -63,7 +63,7 @@ export const ACTORS:Actors[] = [
     age: 23,
     occupation: "Actriz",
     notableWorks: ["Wednesday", "Scream VI", "X"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Jenna_Ortega_2022.jpg"
+    image: "https://es.web.img3.acsta.net/c_310_420/pictures/20/01/07/10/30/2919303.jpg"
   },
   {
     id: 9,
@@ -71,7 +71,7 @@ export const ACTORS:Actors[] = [
     age: 32,
     occupation: "Actor",
     notableWorks: ["Saltburn", "The Banshees of Inisherin", "Eternals"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Barry_Keoghan_2019.jpg"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Barry_Keoghan_at_the_2024_Toronto_International_Film_Festival_%28cropped%29.jpg/1200px-Barry_Keoghan_at_the_2024_Toronto_International_Film_Festival_%28cropped%29.jpg"
   },
   {
     id: 10,
@@ -79,7 +79,7 @@ export const ACTORS:Actors[] = [
     age: 27,
     occupation: "Actriz y modelo",
     notableWorks: ["Euphoria", "The Hunger Games: The Ballad of Songbirds & Snakes"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Hunter_Schafer_2019.jpg"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZfy_ddkROwppF5ZT6_5bRZ3ZZrVbTiqOszQ&s"
   },
   {
     id: 11,
@@ -87,7 +87,7 @@ export const ACTORS:Actors[] = [
     age: 29,
     occupation: "Actor",
     notableWorks: ["Normal People", "Aftersun", "Gladiator II"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Paul_Mescal_2020.jpg"
+    image: "https://cdn.britannica.com/00/250500-050-47CB256E/Paul-Mescal-2022.jpg"
   },
   {
     id: 12,
@@ -95,7 +95,7 @@ export const ACTORS:Actors[] = [
     age: 28,
     occupation: "Actriz",
     notableWorks: ["Euphoria", "The White Lotus", "Anyone But You"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/8/83/Sydney_Sweeney_2019.jpg"
+    image: "https://m.media-amazon.com/images/M/MV5BZTIwYmNjMjctYTAyZi00Njg0LWJmZWQtZTc1MmZjZjBiMmQ4XkEyXkFqcGc@._V1_.jpg"
   },
   {
     id: 13,
@@ -103,7 +103,7 @@ export const ACTORS:Actors[] = [
     age: 35,
     occupation: "Actor",
     notableWorks: ["The Crown", "Challengers", "Emma"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Josh_O%27Connor_2019.jpg"
+    image: "https://m.media-amazon.com/images/M/MV5BMjExNDkwMDIzNF5BMl5BanBnXkFtZTgwMzk0MjA1NDM@._V1_.jpg"
   },
   {
     id: 14,
@@ -111,7 +111,7 @@ export const ACTORS:Actors[] = [
     age: 24,
     occupation: "Actriz y cantante",
     notableWorks: ["West Side Story", "The Hunger Games: The Ballad of Songbirds & Snakes"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Rachel_Zegler_2021.jpg"
+    image: "https://es.web.img2.acsta.net/pictures/22/07/19/10/36/4657071.jpg"
   },
   {
     id: 15,
@@ -119,6 +119,6 @@ export const ACTORS:Actors[] = [
     age: 19,
     occupation: "Actor",
     notableWorks: ["Room", "Wonder", "The Little Mermaid"],
-    image: "https://upload.wikimedia.org/wikipedia/commons/d/d8/Jacob_Tremblay_2019.jpg"
+    image: "https://m.media-amazon.com/images/M/MV5BY2M3ZTQzMGEtMjNmMy00NWZjLTkxNWQtMWM1NmQyODdiN2RjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
   }
 ];
